a223c47bee38b70db9c5d75501acd4a88c7ec10e
chasewaldron2400 <chasew2400@gmail.com>
2025-04-12 18:05:38 -0500

    CSS Changes, Added Swipe to Blog Feed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useState, useRef } from "react";
 
 const Post = ({ images, caption }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -13,12 +17,46 @@ const Post = ({ images, caption }) => {
     );
   };
 
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    const distance = touchStartX.current - touchEndX.current;
+    const threshold = 50; // Minimum swipe distance
+
+    if (distance > threshold) {
+      // Swiped left
+      nextImage();
+    } else if (distance < -threshold) {
+      // Swiped right
+      prevImage();
+    }
+  };
+
   return (
     <div className="post">
-      <div className="image-container">
-        <button className="nav-btn left" onClick={prevImage}>&#8249;</button>
-        <img src={images[currentIndex]} alt="Blog Post" className="post-image" />
-        <button className="nav-btn right" onClick={nextImage}>&#8250;</button>
+      <div
+        className="image-container"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <button className="nav-btn left" onClick={prevImage}>
+          &#8249;
+        </button>
+        <img
+          src={images[currentIndex]}
+          alt="Blog Post"
+          className="post-image"
+        />
+        <button className="nav-btn right" onClick={nextImage}>
+          &#8250;
+        </button>
       </div>
       <p className="caption">{caption}</p>
     </div>
